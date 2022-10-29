13e681a28f4d8b1107b4de33d69ccf3412021755
Jhonatas Souza <jhonatasbs@gmail.com>
2022-10-29 17:20:40 -0300

    Add aditional validations

--- api/serializers.py
+++ api/serializers.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, validator, ValidationError
 from .processors import ImageProcessor
 
 class ImageInformationRequest(BaseModel):
@@ -22,6 +22,11 @@ class ImageCropRequest(BaseModel):
     x1: int
     y0: int
     y1: int
+
+    @validator("*")
+    def validate_coordinates(item):
+        if isinstance(item, int) and item < 0:
+            raise ValueError("Coordinates need to be positive values")
 
 class ImageCropResponse(BaseModel):
     result_image: str
